10a8a19bd0d4664d3c8d118ae35067096d738612
DynamoUltima <dynamo.joey@gmail.com>
2020-12-08 16:20:32 +0000

    footer design

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -1,20 +1,23 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faFacebook} from "@fortawesome/free-brands-svg-icons";
+import footerImg  from '../images/genecon.png'
+
 const Footer = () => {
     return (
-        <footer className="page-footer">
-            <div className="container">
+        <footer className="page-footer  grey darken-3">
+            <div className="container ">
                 <div className="row">
                     <div className="col l6 s12">
-                        <h5 className="white-text">Footer Content</h5>
-                        <p className="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+                        <h5 className="white-text"><img src={footerImg}  height="100px" alt="footer"/></h5>
+                        <p className="grey-text text-lighten-4">Our mission is to engage in the electrical construction of residential, commercial, industrial and community projects with efficient and motivated craftsmen to produce very excellent work to meet customer satisfaction..</p>
                     </div>
                     <div className="col l4 offset-l2 s12">
-                        <h5 className="white-text">Links</h5>
+                        <h5 className="white-text">Facebook Page</h5>
                         <ul>
-                            <li><a href className="waves-effect waves-light btn-floating social facebook">
-<i className="fa fa-facebook"></i> Sign in with facebook</a></li>
-                            <li><a className="grey-text text-lighten-3" href="#!">Link 2</a></li>
-                            <li><a className="grey-text text-lighten-3" href="#!">Link 3</a></li>
-                            <li><a className="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                        <li><a className="grey-text text-lighten-3" href="#!">Check out our Facebook Page on the link below. </a></li>
+                        <div className="section"></div>
+                            <li><div className="container"><a className="center" href="https://www.facebook.com/genecelectrix"><FontAwesomeIcon className ="center" icon={faFacebook} size="1x" />Facebook</a></div></li>
+                            
                         </ul>
                     </div>
                 </div>
